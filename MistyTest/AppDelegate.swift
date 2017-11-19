bcbcfa570b751e08c72eefbca40ad85bb6807e90
Mona Holtmann <mona@p200300612d2c044910e106bd6cca1d7f.dip0.t-ipconnect.de>
2017-11-19 17:05:10 +0100

    Initial Commit

---
+++ MistyTest/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MistyTest
+//
+//  Created by Mona Holtmann on 19.11.17.
+//  Copyright © 2017 Mona Holtmann. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ MistyTest/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MistyTest
+//
+//  Created by Mona Holtmann on 19.11.17.
+//  Copyright © 2017 Mona Holtmann. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
