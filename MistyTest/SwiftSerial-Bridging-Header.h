5f4860dee12cfb4fa67a812a08bae8a4a7214891
DragoTier <ivo.muellner@web.de>
2018-03-29 17:08:25 +0200

    Project + .gitignore
    gitignore kann man im finder nicht sehen

---
+++ MistyTest/SwiftSerial-Bridging-Header.h
@@ -0,0 +1,17 @@
+//
+//  SwiftSerial-Bridging-Header.h
+//  MistyTest
+//
+//  Created by Madita Lange on 25.11.17.
+//  Copyright © 2017 Mona Holtmann. All rights reserved.
+//
+#import "/Users/maddy/Desktop/Jahresprojekt/MistyTest/Pods/ORSSerialPort/Source/ORSSerialPort.h"
+#import "/Users/maddy/Desktop/Jahresprojekt/MistyTest/Pods/ORSSerialPort/Source/ORSSerialPortManager.h"
+
+#ifndef SwiftSerial_Bridging_Header_h
+#define SwiftSerial_Bridging_Header_h
+
+
+#endif /* SwiftSerial_Bridging_Header_h */
+
+
