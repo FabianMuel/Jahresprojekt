952a1a14f59f1d913f77b1524404eac75cc331ad
DragoTier <ivo.muellner@web.de>
2018-04-27 12:58:00 +0200

    refactored structure

--- JavaBackend/MistyBackend/src/main/Main.java
+++ JavaBackend/MistyBackend/src/main/Main.java
@@ -1,6 +1,10 @@
+package main;
+import view.*;
+
 public class Main {
 
     public static void main(String args[]){
-        new MainView();
+
+        new View();
     }
 }
--- JavaBackend/MistyBackend/src/view/View.java
+++ JavaBackend/MistyBackend/src/view/View.java
@@ -1,15 +1,15 @@
+package view;
+
 import javax.swing.*;
 import java.awt.*;
 
 public class View {
 
-    public static void main(String args[]){
+    public View(){
         JFrame frame = new JFrame("MistyBackend");
         frame.setContentPane(new MainView().panel1);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
-
     }
-
 }
