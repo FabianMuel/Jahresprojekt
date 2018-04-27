b709c500110321ad816381854442da8d08a28570
DragoTier <ivo.muellner@web.de>
2018-04-27 12:28:14 +0200

    created first gui

---
+++ JavaBackend/MistyBackend/src/MainView.java
@@ -0,0 +1,6 @@
+import javax.swing.*;
+
+public class MainView {
+    private JButton button1;
+    public JPanel panel1;
+}
--- JavaBackend/MistyBackend/src/View.java
+++ JavaBackend/MistyBackend/src/View.java
@@ -1,2 +1,15 @@
+import javax.swing.*;
+import java.awt.*;
+
 public class View {
+
+    public static void main(String args[]){
+        JFrame frame = new JFrame("MistyBackend");
+        frame.setContentPane(new MainView().panel1);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+
+    }
+
 }
